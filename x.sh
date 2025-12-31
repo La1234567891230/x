5c142007773023cbe913662527cf58fb4f04d994
La1234567891230 <150842067+La1234567891230@users.noreply.github.com>
2025-12-31 08:56:21 +0100

    Add script to fetch macOS full installer version

---
+++ x.sh
@@ -0,0 +1,1 @@
+softwareupdate --list-full-installers; echo; echo "Please enter version number you wish to download:"; read REPLY; [ -n "$REPLY" ] && softwareupdate --fetch-full-installer --full-installer-version "$REPLY"
